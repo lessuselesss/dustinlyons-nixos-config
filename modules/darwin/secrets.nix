64e3859a8c63d9d5bf286147c89ce77aa4200a17
Ashley Barr <lessuseless@duck.com>
2025-03-10 17:38:56 -0700

    Update secrets.nix to use proper agenix format

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -2,9 +2,31 @@
 
 let user = "lessuseless"; in
 {
-  age.identityPaths = [
-    "/Users/${user}/.ssh/id_ed25519"
-  ];
+  age = { 
+    identityPaths = [ 
+      "/Users/${user}/.ssh/id_ed25519"
+    ];
+
+    secrets = { 
+      "github-ssh-key" = {
+        symlink = true;
+        path = "/Users/${user}/.ssh/id_github";
+        file =  "${secrets}/github-ssh-key.age";
+        mode = "600";
+        owner = "${user}";
+        group = "staff";
+      };
+
+      "github-signing-key" = {
+        symlink = false;
+        path = "/Users/${user}/.ssh/pgp_github.key";
+        file =  "${secrets}/github-signing-key.age";
+        mode = "600";
+        owner = "${user}";
+        group = "staff";
+      };
+    };
+  };
 
   # Your secrets go here
   #
@@ -15,23 +37,4 @@ let user = "lessuseless"; in
   #
   #       If you change the key name, you'll need to update the SSH configuration in shared/home-manager.nix
   #       so Github reads it correctly.
-
-  #
-  # age.secrets."github-ssh-key" = {
-  #   symlink = true;
-  #   path = "/Users/${user}/.ssh/id_github";
-  #   file =  "${secrets}/github-ssh-key.age";
-  #   mode = "600";
-  #   owner = "${user}";
-  #   group = "staff";
-  # };
-
-  # age.secrets."github-signing-key" = {
-  #   symlink = false;
-  #   path = "/Users/${user}/.ssh/pgp_github.key";
-  #   file =  "${secrets}/github-signing-key.age";
-  #   mode = "600";
-  #   owner = "${user}";
-  # };
-
 }
