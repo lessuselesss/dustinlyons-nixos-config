41e9c44a89270fbee32318e45c1e810197514111
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-06-14 01:40:27 -0600

    Update default.nix
    Signed-off-by: lessuseless <179788364+lessuselesss@users.noreply.github.com>

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -49,9 +49,19 @@ user}/.local/share/src/nixos-config:/etc/nixos" ];
     settings = {
       allowed-users = [ "${user}" ];
       trusted-users = [ "@admin" "${user}" ];
-      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
-      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-    };
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+        "https://dustinlyons-nixos-config.cachix.org"
+        "https://lessuseless-nixos-config.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "dustinlyons-nixos-config.cachix.org-1:G+6axanpp47yE5d06WvxpH52qicUc4ym34sMseJBl+E="
+        "lessuseless-nixos-config.cachix.org-1:bTpIPie+wvr5stHDRTAicphiCUrwnUgY84q4cRKkjnw="
+      ];
+  };
 
     package = pkgs.nix;
     extraOptions = ''
