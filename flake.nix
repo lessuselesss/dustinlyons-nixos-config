03bdc477568be847ed64b682e8ad73c98fe83b8b
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-06-16 02:30:32 -0600

    Update flake.nix
    Signed-off-by: lessuseless <179788364+lessuselesss@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixjail = {
-      url = "github:lessuseless/nixjail-flake";
+      url = "github:lessuselesss/nixjail-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
