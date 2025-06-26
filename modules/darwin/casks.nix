2863222e2645743cf15eb9657dbd114dd21f1d06
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-06-26 02:48:35 -0600

    Update casks.nix
    Signed-off-by: lessuseless <179788364+lessuselesss@users.noreply.github.com>

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -1,26 +1,115 @@
-_:
+_: [
+  # ---- AI Tools ----
+  # "claude"
+  # "chatgpt"
+  # "msty"
+  # "ollama"
+  # "lm-studio"
 
-[
-  # Development Tools
-  "homebrew/cask/docker"
-  "visual-studio-code"
+  # ---- Development Tools ----
+  # "homebrew/cask/docker"
+  # "visual-studio-code"
+  # "cursor"
+  # "orbstack" # pre-install/login required
+  # "talon"
+  # "qflipper"
+  # "warp"
+  # "sonic-pi"
+  # "tableplus"
+  # "insomnia"
+  # "postico"
+  # "wireshark"
+  # "kindavim"
+  # "jordanbaird-ice"
+  # "mist-cli"
 
-  # Communication Tools
-  "discord"
-  "notion"
-  "slack"
-  "telegram"
-  "zoom"
+  # ---- Communication Tools ----
+  # "discord"
+  # "loom"
+  # "notion"
+  # "slack"
+  # "telegram"
+  # "zoom"
+  # "beeper" # pre-install/login required
 
-  # Utility Tools
-  "syncthing"
+  # ---- Crypto Tools ----
+  # "ledger-live"
 
-  # Entertainment Tools
-  "vlc"
+  # ---- Utility Tools ----
+  # "appcleaner"
+  # "syncthing"
+  # "secretive"
+  # "spacedrive"
+  # "bazecor"
+  # "qflipper"
+  # "homerow"
+  # "sdformatter"
+  # "hiddenbar"
+  # "karabiner-elements"
+  # "keycastr"
+  # "disk-inventory-x"
+  # "raspberry-pi-imager"
+  # "bettertouchtool"
+  # "screenpipe"
+  # "rclone"
+  # "1password@nightly"
 
-  # Productivity Tools
-  "raycast"
+  # ---- Entertainment Tools ----
+  # "iina"
+  # "vlc"
+  # "transmission"
+  # "steam"
 
-  # Browsers
-  "google-chrome"
+  # ---- Productivity Tools ----
+  # "raycast"
+  # "obsidian"
+  # "asana"
+
+  # ---- Browsers ----
+  # "finicky"
+  # "brave-browser"
+  # "duckduckgo"
+  # "firefox"
+  # "librewolf"
+  # "google-chrome"
+  # "orion"
+
+  # ---- Security Tools ----
+  # "silentknight"
+  # "xprocheck"
+  # "secretive"
+  # "mints"
+  # "ulbow"
+  # "cirrus"
+  # "xattred"
+  # "signet"
+  # "whatsyoursign"
+  # "lulu"
+  # "kextviewr"
+  # "netiquette"
+  # "knockknock"
+  # "taskExplorer"
+  # "reikey"
+  # "blockblock"
+  # "dhs" # dylib hijack scanner
+  # "processmonitor"
+  # "filemonitor"
+  # "dnsmonitor"
+
+  # ---- Virtualization ----
+  # "utm"
+  # "crystalfetch"
+  # "viable"
+
+  # ---- Networking ----
+  # "tailscale" # pre-install/login required
+  # "ngrok"
+  # "wireshark"
+
+  # ---- Document Tools ----
+  # "adobe-acrobat-reader"
+  # "anki"
+
+  # ---- Misc / Other ----
+  # "screenpipe"
 ]
